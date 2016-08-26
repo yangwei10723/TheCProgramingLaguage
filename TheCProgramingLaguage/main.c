bd437c8a2e415878bc9061bbfc6ef96e5c3c452d
杨威 <yangwei@37ren.com>
2016-08-26 11:19:40 +0800

    Initial Commit

---
+++ TheCProgramingLaguage/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TheCProgramingLaguage
+//
+//  Created by yw on 16/8/26.
+//  Copyright © 2016年 yw. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
